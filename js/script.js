7f34b91822e1fb2ff300678b9db8acd72daf720f
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-05-28 15:09:09 +0200

    add capitol js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ document.getElementById('search').addEventListener('click', searchCountries);
 function searchCountries() {
     var countryName = document.getElementById('country-name').value;
     if(!countryName.length) countryName = 'Poland';
-    fetch(url + countryName + capital)
+    fetch(url + countryName)
         .then(function(resp) {
             return resp.json();
         })
@@ -17,7 +17,7 @@ function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item) {
         var liEl = document.createElement('li');
-        liEl.innerText = item.name;
+        liEl.innerText = item.name + " .Capitol: " + item.capitol;
         countriesList.appendChild(liEl);
     });
 }
