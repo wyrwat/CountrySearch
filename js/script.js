a1102f551bfa30655a40073a3cb36198648101e7
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-05-28 15:38:08 +0200

    Add capital of country

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item) {
         var liEl = document.createElement('li');
-        liEl.innerText = item.name;
+        liEl.innerText = item.name + ".Capital: " + item.capital;
         countriesList.appendChild(liEl);
     });
 }
