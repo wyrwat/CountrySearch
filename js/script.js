c0eebe3a3758d1f52f74aa0bd2bf851e6da660b4
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-05-28 15:11:32 +0200

    Change js

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function showCountriesList(resp) {
     countriesList.innerHTML = '';
     resp.forEach(function(item) {
         var liEl = document.createElement('li');
-        liEl.innerText = item.name + " .Capitol: " + item.capitol;
+        liEl.innerText = item.name + " .Capitol: " + item.capital;
         countriesList.appendChild(liEl);
     });
 }
