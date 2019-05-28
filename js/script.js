ba10fc9a549b774de8070fd54a57252ad3fcb469
Tomasz Wyrwa <wyrwa.t@gmail.com>
2019-05-28 14:36:00 +0200

    Add logic

---
+++ js/script.js
@@ -0,0 +1,24 @@
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = document.getElementById('countries');
+
+document.getElementById('search').addEventListener('click', searchCountries);
+
+function searchCountries() {
+    var countryName = document.getElementById('country-name').value;
+    if(!countryName.length) countryName = 'Poland';
+    fetch(url + countryName)
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(showCountriesList);
+}
+
+function showCountriesList(resp) {
+    countriesList.innerHTML = '';
+    resp.forEach(function(item) {
+        var liEl = document.createElement('li');
+        liEl.innerText = item.name;
+        countriesList.appendChild(liEl);
+    });
+}
+
